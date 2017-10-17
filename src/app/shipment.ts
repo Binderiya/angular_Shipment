8497b2691f97f01ed1fbd426fc1fbaae4b386800
Binderiya <binderiya93@gmail.com>
2017-10-17 16:29:17 -0400

    date

--- src/app/shipment.ts
+++ src/app/shipment.ts
@@ -1,7 +1,7 @@
 export class Shipment {
   constructor(public id: number,
-              public pickupDate: string,
-              public deliveryDate: string,
+              public pickupDate: Date,
+              public deliveryDate: Date,
               public origin: string,
               public destination: string,
               public carrier: string,
