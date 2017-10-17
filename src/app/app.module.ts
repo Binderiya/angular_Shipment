f54f2f10eb97e33c7c96c7a1108a450c190cc958
Binderiya <binderiya93@gmail.com>
2017-10-17 16:13:34 -0400

    added conditions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import { NgClass } from '@angular/common';
 import { ShipmentDetailsComponent } from './shipment-details/shipment-details.component';
 @NgModule({
   declarations: [
